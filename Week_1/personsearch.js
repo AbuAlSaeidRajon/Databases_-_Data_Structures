cc91d141c2665c25382312b259164c5cc5394d81
Saeid Md <khanrajon5@gmail.com>
2024-03-22 14:54:00 +0200

    textembeddedsvg file added

--- Week_1/personsearch.js
+++ Week_1/personsearch.js
@@ -16,4 +16,22 @@ function search(searchKey, value){
     return found;
 }
 
+console.log("########");
 console.log(search("firstname", "Mary"));
+console.log("########");
+console.log(search("lastname", "River"));
+console.log("########");
+console.log(search("age", 30));
+console.log("########");
+
+const age=30;
+const result = search("age", age);
+if(result.length>0){
+    console.log("Persons age of 30:");
+    for(const person of result){
+        console.log(`${person.lastname} ${person.firstname}`);
+    }
+}
+else{
+    console.log(`No persons age ${age}`);
+}
